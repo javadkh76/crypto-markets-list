e38e203e4d5478abcfb967ac755b4e1926d7f25f
Mohammad Javad Khoshraftar <khoshraftar.javad@gmail.com>
2023-10-18 19:23:10 +0330

    Add .env.example file and add default api url to maketsThunk

--- src/features/markets/marketsThunk.tsx
+++ src/features/markets/marketsThunk.tsx
@@ -1,6 +1,6 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL || 'https://api.bitpin.ir/v1'
 
 export const getMarkets = createAsyncThunk('markets/getMarkets', async () => {
   const response = await axios.get(`${API_URL}/mkt/markets/`, {})
